929271c60e13ac446b60ef5e95f074c16c3943e8
SabrinaPB07 <sabrinapaulabruno@gamil.com>
2024-02-21 22:30:27 -0300

    lista 1 com 3 exercicios

---
+++ listas/lista1/exercicio3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("insira a quantidade de dinheiro em centavos para a conversão em reais:");
+        int centavos = int.Parse(Console.ReadLine());
+
+        int reais = centavos / 100;
+        int resultado = centavos % 100;
+         
+         Console.WriteLine("O total em dinheiro eh: " + reais + " real(is) e " + resultado + " centavo(s)");
+
+
+    }
+}
