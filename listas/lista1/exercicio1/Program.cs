e2b7e5f26638fcf65dff1f9340fd74f263099f24
SabrinaPB07 <sabrinapaulabruno@gamil.com>
2024-02-21 22:48:31 -0300

    atualizacao das pastas

---
+++ listas/lista1/exercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+ 
+ class Program
+ {
+    static void Main(string[] args) 
+    {
+
+Console.WriteLine("digite a sua primeira nota: ");
+double nota1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("digite a sua segunda nota:");
+double nota2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("digite a sua segunda nota: ");
+double nota3 =  double.Parse(Console.ReadLine());
+
+Console.WriteLine("digite a sua quarta nota:");
+double nota4 = double.Parse(Console.ReadLine());
+
+double mediaDoAluno = (nota1 + nota2 + nota3 + nota4) / 4.0;
+
+Console.WriteLine("A média do aluno eh: " + mediaDoAluno);
+
+    }
+ }
---
+++ listas/lista1/exercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("digite a base do triangulo: ");
+        double baseDoTriangulo = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("digite a altura do triangulo: ");
+        double alturaDoTriangulo = double.Parse(Console.ReadLine());
+
+      double AreaDoTriangulo = (baseDoTriangulo * alturaDoTriangulo) / 2;
+      bool resultado = AreaDoTriangulo > 20;
+
+      Console.WriteLine("A area do triangulo eh maior que 20? " + resultado);
+
+    }
+}
