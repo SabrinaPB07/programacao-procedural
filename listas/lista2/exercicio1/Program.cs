703133cd02feaad694e05eb06bae0bde12c0522b
SabrinaPB07 <sabrinapaulabruno@gamil.com>
2024-02-28 20:19:48 -0300

    lista2

---
+++ listas/lista2/exercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      Console.WriteLine("digite o seu nome de usuario");
+      string usuario = Console.ReadLine();
+
+     Console.WriteLine("insira a sua senha:");
+     string senha = Console.ReadLine();
+
+      if (usuario == "admin" && senha == "123senha")
+      {
+        Console.WriteLine("Login realizado com sucesso!");
+      }
+      else 
+      {
+        Console.WriteLine("credenciais invalidas...");
+      }
+
+    }
+}
---
+++ listas/lista2/exercicio2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("digite a primeira nota:");
+        double nota1 = double.Parse(Console.ReadLine());
+
+         Console.WriteLine("digite a segunda nota:");
+         double nota2 = double.Parse(Console.ReadLine());
+
+          Console.WriteLine("digite a terceira nota:");
+         double nota3 = double.Parse(Console.ReadLine());
+
+          Console.WriteLine("digite a quarta nota:");
+         double nota4 = double.Parse(Console.ReadLine());
+
+         double notaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+
+         if (notaFinal >= 5.0)
+         {
+            Console.WriteLine("Aluno aprovado");
+         }
+         else if (notaFinal < 5.0 && notaFinal >= 3.0)
+         {
+            Console.WriteLine("aluno em recuperacao");
+         }
+        else
+        {
+            Console.WriteLine("Aluno reprovado");
+        }
+
+    }
+}
