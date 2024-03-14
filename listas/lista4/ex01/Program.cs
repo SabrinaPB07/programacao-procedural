3753dc5be48104b6189f878ac192c3495063a026
SabrinaPB07 <sabrinapaulabruno@gamil.com>
2024-03-14 20:29:38 -0300

    lista4

---
+++ listas/lista4/ex01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int contador = 1; contador <= 10; contador++)
+        {
+            for (int contador2 = 1; contador2 <= 10; contador2++)
+            {
+                System.Console.WriteLine( contador + " X " + contador2 + " = " + contador * contador2);
+                
+            }
+            System.Console.WriteLine("\n");
+        }
+    }
+}
---
+++ listas/lista4/ex02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        const int MENOR_TAMANHO = 1;
+        const int MAIOR_TAMANHO =9;
+
+        for ( int contador = 1; contador == contador; contador++)
+        {
+            System.Console.WriteLine("digite a altura desejda do triangulo retangulo: ");
+            int alturaDoTrianguloRetangulo = int.Parse(Console.ReadLine());
+
+            if (alturaDoTrianguloRetangulo >= MENOR_TAMANHO && alturaDoTrianguloRetangulo <= MAIOR_TAMANHO)
+            {
+                for (int linha = 1; linha <= alturaDoTrianguloRetangulo; linha++)
+                {
+                    for ( int coluna = 1; coluna <= alturaDoTrianguloRetangulo; coluna++)
+                    {
+                        if (coluna <= linha)
+                        {
+                            Console.Write(coluna);
+                        }
+                    }
+                    System.Console.WriteLine();
+                }
+                break;
+            } 
+            else
+            {
+                System.Console.WriteLine("altura invalida, digite uma altura de 1 a 9");
+            }
+        }
+    }
+}
